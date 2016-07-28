e84eb9861bac4ff1eafcb9e14de44ca2e8664db6
Ahmad Kandil <kandileng@gmail.com>
2016-07-28 22:10:15 +0200

    Selection Sort

--- Selection  Sort.cpp
+++ Selection  Sort.cpp
@@ -4,22 +4,24 @@
 using namespace std;
 
 void swap(int &a,int &b){
-    a=a+b,b=a-b,a=a-b;
+    a+=b,b=a-b,a -=b;
 }
 
 void sort(int *index,int size){
     int i,j,min,s;
     for(i=0;i<size-1;++i){
-        min = *(index+i);
+        min = *(index+i),s=i;
         for(j=i+1;j<size;++j)
-            if(min>*(index+j))
+            if(min>(*(index+j)))
                 min = *(index+j),s = j;
 
-        swap(*(index+i),*(index+s));
+        if(s!=i)
+            swap(*(index+i),*(index+s));
     }
 }
 
-int main(){
+int main()
+{
     int n=0; cin>>n;
     int arr[n];
     for(int i=0;i<n;++i) cin>>arr[i];
