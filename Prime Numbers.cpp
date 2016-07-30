a3b224036f457c2d3968b8ca3f0fb0cd0901e491
Ahmad Kandil <kandileng@gmail.com>
2016-07-30 04:17:12 +0200

    Prime Numbers

---
+++ Prime Numbers.cpp
@@ -0,0 +1,18 @@
+//In The Name of Allah
+#include <iostream>
+
+bool IS_Prime(int n){
+    if(n==1||n==2||n==3)
+        return true;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+int main(){
+    int n; std::cin>>n;
+    std::cout << IS_Prime(n);
+ return 0;
+}
