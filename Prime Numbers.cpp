018611b6e074c3424e636219b363382a82457633
Ahmad Kandil <kandileng@gmail.com>
2016-07-30 04:18:03 +0200

    Prime Numbers

--- Prime Numbers.cpp
+++ Prime Numbers.cpp
@@ -4,10 +4,10 @@
 bool IS_Prime(int n){
     if(n==1||n==2||n==3)
         return true;
-    for(int i=2;i*i<=n;i++){
+    for(int i=2;i*i<=n;i++)
         if(n%i==0)
             return false;
-    }
+    
     return true;
 }
 
