2832e5ed910e7de3348935df3ad48b0739cdc0e9
Ahmad Kandil <kandileng@gmail.com>
2016-08-28 03:22:08 +0200

    LCM

---
+++ LCM.cpp
@@ -0,0 +1,17 @@
+//IN THE NAME OF ALLAH
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b){
+	if (b == 0) return a;
+	return gcd(b, a%b);
+}
+
+int lcm(int a, int b){
+	return a*b / gcd(a, b);
+}
+
+int main(){
+	cout << lcm(49, 10) << endl;
+	return 0;
+}
