a0b322adf876aa1803967716ec0c730b2ee82984
Ahmad Kandil <kandileng@gmail.com>
2016-12-22 07:51:14 -0800

    Fibonacci

--- Dynamic Programming/01- Fibonacci.cpp
+++ Dynamic Programming/01- Fibonacci.cpp
@@ -7,7 +7,7 @@ const int mx = 1002;
 ll Fib[mx];
 ll Fibonacci(int n) {
 	Fib[0] = 0, Fib[1] = 1;
-	for (int i = 2; i <= n; i++){
+	for (int i = 2; i <= n; i++) {
 		Fib[i] = Fib[i - 1] + Fib[i - 2];
 	}
 	return Fib[n];
