d90853ed63fe2999d54bc8c11bce3d3b53b459d3
Ahmad Kandil <kandileng@gmail.com>
2016-12-28 03:38:20 +0200

    Linear Search

--- Searching/01- Linear Search.cpp
+++ Searching/01- Linear Search.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 void search(int arr[], int key, int size) {
-    for(int i = 0; i < size; i++)
-        if(key == arr[i]) {
-            cout << key << " present at index : " <<  i<< endl;
-            return;
-        }
-    cout << "not found" << endl;
+	for (int i = 0; i < size; i++)
+	if (key == arr[i]) {
+		cout << key << " present at index : " << i << endl;
+		return;
+	}
+	cout << "not found" << endl;
 }
 
 int main() {
-    int arr[10] = {1, 2, 5, 3, 7, 8, 9, 4, 2, 1}, key;
-    cout << "Enter key : "; cin >> key;
-    search(arr, key, 10);
-    return 0;
+	int arr[10] = { 1, 2, 5, 3, 7, 8, 9, 4, 2, 1 }, key;
+	cout << "Enter key : "; cin >> key;
+	search(arr, key, 10);
+	return 0;
 }
