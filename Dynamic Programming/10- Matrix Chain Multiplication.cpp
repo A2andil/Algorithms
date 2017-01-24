8643366bdedf9a3c4df91c2677ae9391c89f3251
Ahmad Kandil <kandileng@gmail.com>
2017-01-24 06:06:26 +0200

    Matrix Chain Multiplication

--- Dynamic Programming/10- Matrix Chain Multiplication.cpp
+++ Dynamic Programming/10- Matrix Chain Multiplication.cpp
@@ -3,22 +3,22 @@
 #include <limits.h>
 
 int MatrixChain(int p[], int n) {
-    int m[n][n];
+    int dp[n][n];
     int i, j, k, L, q;
-    for (i = 1; i < n; i++) m[i][i] = 0;
+    for (i = 1; i < n; i++) dp[i][i] = 0;
 
     for (L = 2; L < n; L++) {
         for (i = 1; i < n - L + 1; i++) {
             j = i + L - 1;
-            m[i][j] = INT_MAX;
+            dp[i][j] = INT_MAX;
             for (k = i; k <= j - 1; k++) {
-                q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-                if (q < m[i][j])
-                    m[i][j] = q;
+                q = dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j];
+                if (q < dp[i][j])
+                    dp[i][j] = q;
             }
         }
     }
-    return m[1][n - 1];
+    return dp[1][n - 1];
 }
 
 int main() {
