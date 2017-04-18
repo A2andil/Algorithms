d651de448dc349997f37b1570d71f5ca5b7ce88e
Ahmad Kandil <kandileng@gmail.com>
2017-04-18 17:43:43 +0200

    BFS

---
+++ Graph/02- Breadth First Search.cpp
@@ -0,0 +1,34 @@
+//in the name of God
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+#define ll long long
+using namespace std;
+
+const int N = 5;
+vector<int> list[N];
+int visited[N], parent[N];
+queue<int> q;
+
+void bfs(int s) {
+        q.push(s);
+        parent[s] = 0;
+        while (!q.empty()) {
+                visited[s] = 1, s = q.front();
+                q.pop();
+                for (auto x : list[s]) {
+                        if (!visited[x]) {
+                                parent[x] = parent[s] + 1;
+                                q.push(x);
+                        }
+                }
+        }
+}
+
+int main() {
+
+        return 0;
+}
