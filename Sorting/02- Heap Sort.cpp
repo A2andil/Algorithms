a39eb61d76e95c7d734e815eafd48125ba35e2d1
Ahmad Kandil <kandileng@gmail.com>
2017-06-16 18:45:30 +0200

    Heap Sort

--- Sorting/02- Heap Sort.cpp
+++ Sorting/02- Heap Sort.cpp
@@ -1,4 +1,4 @@
-//in the name of God
+// in the name of God
 
 #include <iostream>
 #include <limits>
@@ -8,76 +8,63 @@ using namespace std;
 
 class MinHeap {
 private:
-        ll *list, capacity, size;
-        void MinHeapify(int i) {
-                int l = 2 * i + 1;
-                int r = 2 * i + 2;
-                int smallest = i;
-                if (l < size && list[l] < list[i])
-                        smallest = l;
-                if (r < size && list[r] < list[smallest])
-                        smallest = r;
-                if (smallest != i) {
-                        swap(list[i], list[smallest]);
-                        MinHeapify(smallest);
-                }
-        }
+	ll *list, capacity, size;
+	void minHeapify(int i) {
+		int l = 2 * i + 1, r = 2 * i + 2, idx;
+		idx = l < size && list[l] < list[i] ? l : i;
+		idx = r < size && list[r] < list[idx] ? r : idx;
+		if (idx != i) {
+			swap(list[i], list[idx]);
+			minHeapify(idx);
+		}
+	}
 public:
-        MinHeap(int sz) {
-                size = 0;
-                capacity = sz;
-                list = new ll[sz];
-        }
-        ll ExtractMin() {
-                if (size <= 0)
-                        return INT_MAX;
-                if (size == 1) {
-                        size = size - 1;
-                        return list[0];
-                }
-                ll root = list[0];
-                list[0] = list[size - 1];
-                size = size - 1;
-                MinHeapify(0);
-                return root;
-        }
-        void InsertKey(ll k) {
-                if (size == capacity) {
-                        cout << "Overflow: Could not insertKey" << endl;
-                        return;
-                }
-                size = size + 1;
-                int i = size - 1;
-                list[i] = k;
-                while (i != 0 && list[(i - 1) / 2] > list[i]) {
-                        swap(list[i], list[(i - 1) / 2]);
-                        i = (i - 1) / 2;
-                }
-        }
+	MinHeap(int sz) {
+		size = 0, capacity = sz, list = new ll[sz];
+	}
+	ll extractMin() {
+		if (size <= 0) return INT_MAX;
+		else if (size == 1) {
+			size = 0;
+			return list[0];
+		}
+		ll root = list[0];
+		list[0] = list[size - 1], size = size - 1;
+		minHeapify(0);
+		return root;
+	}
+	void insertKey(ll k) {
+		int i = size;
+		list[i] = k, size = size + 1;
+		while (i != 0 && list[(i - 1) / 2] > list[i]) {
+			swap(list[i], list[(i - 1) / 2]);
+			i = (i - 1) / 2;
+		}
+	}
 };
 
 class HeapSort {
 private:
-        MinHeap *hp;
+	MinHeap *hp;
 public:
-        HeapSort(ll list[], int sz) {
-                hp = new MinHeap(sz);
-                for (int i = 0; i < sz; i++) {
-                        hp->InsertKey(list[i]);
-                }
-                for (int i = 0; i < sz; i++) {
-                        list[i] = hp->ExtractMin();
-                }
-        }
+	HeapSort(ll list[], int sz) {
+		hp = new MinHeap(sz);
+		for (int i = 0; i < sz; i++) {
+			hp->insertKey(list[i]);
+		}
+		for (int i = 0; i < sz; i++) {
+			list[i] = hp->extractMin();
+		}
+	}
 };
 
 int main() {
-        ll list[] = { 1, 5, -1, 20, 0, 5 };
-        int sz = sizeof(list) / sizeof(ll);
-        HeapSort *hSort = new HeapSort(list, sz);
-        for (int i = 0; i < sz; i++) {
-                cout << list[i] << ' ';
-        }
-        cout << endl;
-        return 0;
+	ll list[] = { 1, 5, -1, 20, 0, 5 };
+	int sz = sizeof(list) / sizeof(ll);
+	HeapSort *hSort = new HeapSort(list, sz);
+	for (int i = 0; i < sz; i++) {
+		cout << list[i] << ' ';
+	}
+	cout << endl;
+	return 0;
 }
