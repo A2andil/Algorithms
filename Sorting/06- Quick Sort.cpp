10b237f097f2bcd3e986ad40be70bb05d8e72dd6
Ahmad Kandil <kandileng@gmail.com>
2017-06-24 23:04:55 +0200

    Sorting

---
+++ Sorting/06- Quick Sort.cpp
@@ -0,0 +1,36 @@
+// in the name of God
+
+#include <iostream>
+#include <vector>
+
+#define ll long long
+using namespace std;
+
+vector<int> list;
+
+int partion(int lw, int hi) {
+        int x = list[hi], i = lw - 1;
+        for (int j = lw; j < hi - 1; j += 1) {
+                if (list[j] <= x) {
+                        i += 1;
+                        swap(list[j], list[i]);
+                }
+        }
+        if (list[i + 1] > list[hi]) {
+                swap(list[hi], list[i + lw]);
+                return i + 1;
+        }
+        return hi;
+}
+
+void quick_sort(int lw, int hi) {
+        if (lw < hi) {
+                int idx = partion(lw, hi);
+                quick_sort(lw, idx - 1);
+                quick_sort(idx + 1, hi);
+        }
+}
+
+int main() {
+        return 0;
+}
---
+++ Sorting/07- Count Sort.cpp
@@ -0,0 +1,37 @@
+// in the name of God
+
+#include <iostream>
+#include <vector>
+
+#define ll long long
+#define uint unsigned int
+using namespace std;
+
+vector<int> list;
+
+void counting_sort() {
+    int mx = 0;
+    for (uint i = 0; i < list.size(); i++) {
+        mx = max(mx, list[i]);
+    }
+    int count[mx + 1], sorted_list[list.size()];
+
+    // count numbers
+    for (int i = 0; i <= mx; i++)
+        count[i] = 0;
+    for (uint j = 0; j < list.size(); j++)
+        count[list[j]] += 1;
+
+    // prefix sum
+    for (int i = 1; i <= mx; i++)
+        count[i] = count[i] + count[i - 1];
+
+    for (uint j = 0; j < list.size(); j++) {
+        sorted_list[count[list[j]] - 1] = list[j];
+        count[list[j]] -= 1;
+    }
+}
+
+int main() {
+    return 0;
+}
