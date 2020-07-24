fed6951be7893bbb111595e0b492eb524938bc5f
Ahmad Kandil <kandileng@gmail.com>
2020-07-24 07:00:58 +0200

    Sieve

---
+++ General/Sieve.cpp
@@ -0,0 +1,11 @@
+void seive(int n) { 
+    bool prime[n + 1]; 
+    memset(prime, true, sizeof(prime)); 
+  
+    for (int p = 2; p * p <= n; p++) { 
+        if (prime[p] == true) {  
+            for (int i = p * p; i <= n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+} 
